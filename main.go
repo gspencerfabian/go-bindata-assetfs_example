4c47e270a5a19cf65a7ec4ef4744806709dcb4ef
G Spencer Fabian <spencer@gsfabian.com>
2017-04-22 18:09:24 -0700

    fixing file server issue

--- main.go
+++ main.go
@@ -8,14 +8,16 @@ import (
 var err error
 
 func main() {
-	http.Handle("/static/", http.FileServer(assetFS()))
-	http.HandleFunc("/", userHandle)
-	http.ListenAndServe(":8080", nil)
+	r := http.NewServeMux()
+	r.HandleFunc("/", indexHandle)
+	r.Handle("/static/", http.StripPrefix("/static/", http.FileServer(assetFS())))
+	http.ListenAndServe(":8080", r)
 }
 
-func userHandle(w http.ResponseWriter, r *http.Request) {
-	user := User{Name: "Spencer", Photo: "/img/image.jpg"}
-	t := template.Must(template.New("index.html").ParseFiles("./data/index.html"))
+func indexHandle(w http.ResponseWriter, r *http.Request) {
+	user := User{Name: "Spencer", Photo: "static/img/image.jpg"}
+	tfile := MustAsset("data/templates/index.html")
+	t := template.Must(template.New("index.html").Parse(string(tfile)))
 	err = t.Execute(w, user)
 	if err != nil {
 		panic(err)
